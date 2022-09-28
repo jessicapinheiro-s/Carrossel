7629542c01fd1bfcfbc1d6fc98f00938fa4b4ed9
Jessica Pinheiro <jessicasilva.js1314@gmail.com>
2022-09-28 16:41:53 -0300

    subindo js

--- Slide de imagens/app.js
+++ Slide de imagens/app.js
@@ -1,25 +1,51 @@
-
-let images = document.querySelectorAll('.slides .imgs img');
-let allImages = images.length;
+let balls = document.querySelector('.balls');
+let atual = 0;
+let images = document.querySelectorAll('.slides .imgs');
 let firstImg = document.getElementById('primeiraImg');
 
 
+for (let i = 0; i < images.length; i++) {
+    let div = document.createElement('div');
+    div.id = i;
+    balls.appendChild(div);
+}
+
+document.getElementById('0').classList.add('imgAtual');
+
+let pos = document.querySelectorAll('.balls div');
+
+
+for (let i = 0; i < pos.length; i++) {
+    pos[i].addEventListener('click', () => {
+        atual = pos[i].id;
+        slide();
+    })
+}
+
 document.getElementById('bt-voltar').addEventListener('click', function () {
-    for (let i = 0; i < allImages; i++) {
-        if(firstImg){
-            alert('nao');
-        }else{
-            images[i].classList.add('toRight');
-        }
-        
-    }
+    atual--;
+    slide();
 })
 
 
 document.getElementById('bt-prox').addEventListener('click', function () {
-    for (let i = 0; i < allImages; i++) {
-        images[i].classList.add('toLeft');
-    }
+    atual++;
+    slide();
 })
 
+function slide() {
+    if (atual >= images.length) {
+        atual = 0;
+    } else if (atual < 0) {
+        atual = images.length - 1;
+    }
+    document.querySelector('.imgAtual').classList.remove('.imgAtual');
+    if (window.matchMedia("(min-width:1300px)").matches) {
+        firstImg.style.marginLeft = -1000 * atual + 'px';
+    } else if (window.matchMedia("(min-width:1000px)").matches) {
+        firstImg.style.marginLeft = -1100 * atual + 'px';
+    }
+    document.getElementById('primeiraImg').classList.add('.imgAtual');
+}
+slide();
 
